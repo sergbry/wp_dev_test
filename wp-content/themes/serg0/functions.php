6e3681e98fbe10cd6c4f72640853a9d51397f59a
Сергей Брыляков <brilyakov.s.a@gmail.com>
2021-02-28 00:08:28 +0200

    + theme CSS & JS

---
+++ wp-content/themes/serg0/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+//подключение стилей темы
+add_action( 'wp_enqueue_scripts', 'theme_stylе' );
+
+function theme_stylе() {
+
+    wp_enqueue_style('mine-style', get_template_directory_uri().'/css/style.css' );
+    wp_enqueue_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css' );
+
+    wp_enqueue_script('jquery-min',get_template_directory_uri().'/js/jquery.min.js');
+    wp_enqueue_script('easing',get_template_directory_uri().'/js/easing.js');
+    wp_enqueue_script('move-top',get_template_directory_uri().'/js/move-top.js');
+
+//
+}
--- wp-content/themes/serg0/index.php
+++ wp-content/themes/serg0/index.php
@@ -1,3 +1,7 @@
 <?php
+wp_head();
 ?>
 <h1>Hello OSI</h1>
+<?php
+wp_footer();
+?>
