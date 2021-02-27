a243ec90bf14b6f62580d268234ddb194e3ae144
Сергей Брыляков <brilyakov.s.a@gmail.com>
2021-02-28 01:21:07 +0200

    + header.php

--- wp-content/themes/serg0/functions.php
+++ wp-content/themes/serg0/functions.php
@@ -5,8 +5,8 @@ add_action( 'wp_enqueue_scripts', 'theme_stylе' );
 
 function theme_stylе() {
 
-    wp_enqueue_style('mine-style', get_template_directory_uri().'/css/style.css' );
     wp_enqueue_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css' );
+    wp_enqueue_style('mine-style', get_template_directory_uri().'/css/style.css' );
 
     wp_enqueue_script('jquery-min',get_template_directory_uri().'/js/jquery.min.js');
     wp_enqueue_script('easing',get_template_directory_uri().'/js/easing.js');
---
+++ wp-content/themes/serg0/header.php
@@ -0,0 +1,86 @@
+<?php
+
+?>
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>
+    <?php
+        bloginfo('name');
+        echo " | ";
+        if (is_home()) {
+            echo "Home";
+        } else {
+            wp_title('');
+        }
+        echo ' :: w3layouts';
+    ?>
+
+</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Personal Blog Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design"
+    />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!----webfonts---->
+    <link href='http://fonts.googleapis.com/css?family=Oswald:100,400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,300italic' rel='stylesheet' type='text/css'>
+    <!----//webfonts---->
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event){
+                event.preventDefault();
+                $('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+            });
+        });
+    </script>
+    <!---->
+
+<?php wp_head(); ?>
+
+</head>
+
+<body>
+
+<!---header---->
+<div class="header">
+    <div class="container">
+        <div class="logo">
+            <a href="/"><img src="<?=get_template_directory_uri();?>/images/logo.jpg" title=""></a>
+        </div>
+        <!---start-top-nav---->
+        <div class="top-menu">
+            <div class="search">
+                <form>
+                    <input type="text" placeholder="" required="">
+                    <input type="submit" value="">
+                </form>
+            </div>
+            <span class="menu"> </span>
+            <ul>
+                <li class="active"><a href="index.html">HOME</a></li>
+                <li><a href="about.html">ABOUT</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+                <div class="clearfix"> </div>
+            </ul>
+        </div>
+        <div class="clearfix"></div>
+        <script>
+            $("span.menu").click(function(){
+                $(".top-menu ul").slideToggle("slow" , function(){
+                });
+            });
+        </script>
+        <!---//End-top-nav---->
+    </div>
+</div>
+<!--/header-->
