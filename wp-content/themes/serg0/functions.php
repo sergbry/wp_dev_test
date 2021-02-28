f7ce8404d98efef5e4aa90916038cc09ddd2d865
Сергей Брыляков <brilyakov.s.a@gmail.com>
2021-02-28 10:40:12 +0200

    + widgets + thumbnails + index main section

--- wp-content/themes/serg0/functions.php
+++ wp-content/themes/serg0/functions.php
@@ -2,11 +2,9 @@
 
 add_action( 'wp_enqueue_scripts', 'theme_stylе' ); // Подключение стилей темы
 add_action( 'after_setup_theme', 'theme_menu' ); // Подключение меню
+add_action( 'widgets_init', 'theme_widgets' ); // Регистрация сайдбара
 
-
-function theme_menu() {
-    register_nav_menu( 'primary', 'Primary Menu' );
-}
+add_theme_support( 'post-thumbnails' ); // Позволяем добавлять миниатюры постов
 
 function theme_stylе() {
 
@@ -17,5 +15,18 @@ function theme_stylе() {
     wp_enqueue_script('easing',get_template_directory_uri().'/js/easing.js');
     wp_enqueue_script('move-top',get_template_directory_uri().'/js/move-top.js');
 
-//
+}
+
+function theme_menu() {
+    register_nav_menu( 'primary', 'Primary Menu' );
+}
+
+function theme_widgets() {
+    register_sidebar( array(
+        'name' => "Right sidebar",
+        'id' => 'right-sidebar',
+        'description' => 'Add widgets to right sidebar',
+        'before_title' => '',
+        'after_title' => ''
+    ) );
 }
